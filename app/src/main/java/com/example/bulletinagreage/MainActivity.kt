fcfbf026d044e4c55494b28009f03d8b606e9c07
mumjvbvb-alt <mumjvbvb@gmail.com>
2026-09-20 22:37:11 +0100

    Fix initial activity dependency

--- app/src/main/java/com/example/bulletinagreage/MainActivity.kt
+++ app/src/main/java/com/example/bulletinagreage/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.bulletinagreage
 
+import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
